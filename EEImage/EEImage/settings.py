48b9e517aca03b276c3870c695d6e4f67c9c9dbd
oreade16 github <oreade1608@gmail.com>
2022-07-08 20:27:50 -0700

    second_commit

--- EEImage/EEImage/settings.py
+++ EEImage/EEImage/settings.py
@@ -21,9 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj4pb7jyiif4s-d+t-xazg252g3ij%0qsg*%jivw3*@otk!4@*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
